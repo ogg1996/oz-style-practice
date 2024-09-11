2ae56969b84d3d2b2383f7091d42c86aa5969fdd
OhGeonGuk <rjsrnr43@naver.com>
2024-09-11 15:48:28 +0900

    css스타일 styled-components로 변환 완료 css 파일들 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
+import styled, { createGlobalStyle } from "styled-components";
 import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
 
+const GlobalStyle = createGlobalStyle`
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  color: white;
+  }
+
+  body {
+    background-color: #121212;
+  }
+`;
+
+const SectionStyle = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 20px 40px;
+`;
+
 function App() {
   return (
-    <main>
-      <Header />
-      <section>
-        {contents.map((el) => (
-          <Content key={el.id} content={el} />
-        ))}
-      </section>
-    </main>
+    <>
+      <GlobalStyle />
+      <main>
+        <Header />
+        <SectionStyle>
+          {contents.map((el) => (
+            <Content key={el.id} content={el} />
+          ))}
+        </SectionStyle>
+      </main>
+    </>
   );
 }
 
